c9138b6ac7b2de7a689645c62732439feb80c9af
tata-antares <tata.antares@yandex.ru>
2016-02-17 16:05:21 +0100

    add CVM test

---
+++ python/utils.py
@@ -0,0 +1,75 @@
+import numpy
+from sklearn.metrics import roc_curve, auc
+
+
+def __rolling_window(data, window_size):
+    """
+    Rolling window: take window with definite size through the array
+
+    :param data: array-like
+    :param window_size: size
+    :return: the sequence of windows
+
+    Example: data = array(1, 2, 3, 4, 5, 6), window_size = 4
+        Then this function return array(array(1, 2, 3, 4), array(2, 3, 4, 5), array(3, 4, 5, 6))
+    """
+    shape = data.shape[:-1] + (data.shape[-1] - window_size + 1, window_size)
+    strides = data.strides + (data.strides[-1],)
+    return numpy.lib.stride_tricks.as_strided(data, shape=shape, strides=strides)
+
+
+def __cvm(subindices, total_events):
+    """
+    Compute Cramer-von Mises metric.
+    Compared two distributions, where first is subset of second one.
+    Assuming that second is ordered by ascending
+
+    :param subindices: indices of events which will be associated with the first distribution
+    :param total_events: count of events in the second distribution
+    :return: cvm metric
+    """
+    # here we compute the same expression (using algebraic expressions for them).
+    n_subindices = float(len(subindices))
+    subindices = numpy.array([0] + sorted(subindices) + [total_events], dtype='int')
+    # via sum of the first squares
+    summand1 = total_events * (total_events + 1) * (total_events + 0.5) / 3. / (total_events ** 3)
+    left_positions = subindices[:-1]
+    right_positions = subindices[1:]
+
+    values = numpy.arange(len(subindices) - 1)
+
+    summand2 = values * (right_positions * (right_positions + 1) - left_positions * (left_positions + 1)) / 2
+    summand2 = summand2.sum() * 1. / (n_subindices * total_events * total_events)
+
+    summand3 = (right_positions - left_positions) * values ** 2
+    summand3 = summand3.sum() * 1. / (n_subindices * n_subindices * total_events)
+
+    return summand1 + summand3 - 2 * summand2
+
+
+def compute_cvm(predictions, masses, n_neighbours=200, step=50):
+    """
+    Computing Cramer-von Mises (cvm) metric on background events: take average of cvms calculated for each mass bin.
+    In each mass bin global prediction's cdf is compared to prediction's cdf in mass bin.
+
+    :param predictions: array-like, predictions
+    :param masses: array-like, in case of Kaggle tau23mu this is reconstructed mass
+    :param n_neighbours: count of neighbours for event to define mass bin
+    :param step: step through sorted mass-array to define next center of bin
+    :return: average cvm value
+    """
+    predictions = numpy.array(predictions)
+    masses = numpy.array(masses)
+    assert len(predictions) == len(masses)
+
+    # First, reorder by masses
+    predictions = predictions[numpy.argsort(masses)]
+
+    # Second, replace probabilities with order of probability among other events
+    predictions = numpy.argsort(numpy.argsort(predictions))
+
+    # Now, each window forms a group, and we can compute contribution of each group to CvM
+    cvms = []
+    for window in __rolling_window(predictions, window_size=n_neighbours)[::step]:
+        cvms.append(__cvm(subindices=window, total_events=len(predictions)))
+    return numpy.mean(cvms)
